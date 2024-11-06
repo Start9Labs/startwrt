1d14b5d5a060394e7ada4f87f07479b836d7cb02
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2024-11-06 13:34:02 -0700

    add network service

---
+++ ui/src/services/network.service.ts
@@ -0,0 +1,9 @@
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NetworkService {
+
+}
