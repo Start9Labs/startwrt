69f4bbaee03b84bfb4f556d8ff6d8d6b868044dc
Sam Sartor <me@samsartor.com>
2025-03-11 23:24:10 -0600

    ucedit seems to work perfect!

--- uciedit/src/lib.rs
+++ uciedit/src/lib.rs
@@ -1,9 +1,12 @@
-use eyre::{bail, eyre, Error};
+pub use eyre::{bail, eyre as error, Error};
 use inpt::split::{Quoted, SingleQuoted, Word};
 use inpt::{inpt, inpt_step, Inpt, InptStep};
-use std::fmt;
-use std::io::{BufRead, BufWriter, Write};
+use std::io::{BufRead, BufWriter};
 use std::{borrow::Cow, fs::File, path::Path};
+use std::{fmt, fs};
+pub use uciedit_macros::UciSection;
+
+extern crate self as uciedit;
 
 #[derive(Inpt, Clone, Copy)]
 pub enum Token<'a> {
@@ -12,13 +15,40 @@ pub enum Token<'a> {
     W(Word<&'a str>),
 }
 
-impl Token<'_> {
-    fn as_str(&self) -> Cow<str> {
+impl<'a> Token<'a> {
+    pub fn as_str(&self) -> Cow<str> {
         // TODO: inpt doesn't currently do unescaping
         match self {
             Token::Q(x) => x.unescape(),
             Token::Sq(x) => x.unescape(),
             Token::W(x) => Cow::Borrowed(x.inner),
+        }
+    }
+
+    pub fn from_display(s: &impl fmt::Display, arena: &'a Arena) -> Self {
+        Self::from_string(s.to_string(), arena)
+    }
+
+    pub fn from_string(s: String, arena: &'a Arena) -> Self {
+        if s.contains(|c: char| c.is_whitespace()) {
+            let q = arena.alloc(format!("{:?}", s));
+            Token::Q(Quoted {
+                inner: &q[1..q.len() - 1],
+            })
+        } else {
+            let s = arena.alloc(s);
+            Token::W(Word { inner: s })
+        }
+    }
+
+    pub fn from_str(s: &'a str, arena: &'a Arena) -> Self {
+        if s.contains(|c: char| c.is_whitespace()) {
+            let q = arena.alloc(format!("{:?}", s));
+            Token::Q(Quoted {
+                inner: &q[1..q.len() - 1],
+            })
+        } else {
+            Token::W(Word { inner: s })
         }
     }
 }
@@ -48,13 +78,14 @@ pub enum Line<'a> {
         list: Token<'a>,
         item: Token<'a>,
     },
+    Skip,
 }
 
 impl fmt::Display for Line<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Line::Empty => writeln!(f),
-            Line::Comment(text) => writeln!(f, "#{}", text),
+            Line::Comment(text) => writeln!(f, "{}", text),
             Line::Section { ty, name: None } => writeln!(f, "config {}", ty),
             Line::Section {
                 ty,
@@ -62,6 +93,7 @@ impl fmt::Display for Line<'_> {
             } => writeln!(f, "config {} {}", ty, name),
             Line::Option { option, value } => writeln!(f, "\toption {} {}", option, value),
             Line::List { list, item } => writeln!(f, "\tlist {} {}", list, item),
+            Line::Skip => Ok(()),
         }
     }
 }
@@ -72,8 +104,8 @@ impl<'a> Line<'a> {
         if rest.is_empty() {
             return Ok(Line::Empty);
         }
-        if let Some(rest) = rest.strip_prefix("#") {
-            return Ok(Line::Comment(rest));
+        if rest.starts_with('#') {
+            return Ok(Line::Comment(line));
         }
         let InptStep {
             data: Ok(keyword),
@@ -84,22 +116,28 @@ impl<'a> Line<'a> {
         };
         Ok(match &*keyword.as_str() {
             "config" => {
-                let (ty, name): (Token, Token) =
-                    inpt(rest).map_err(|err| eyre!("could not parse section: {err}"))?;
-                let name: Option<_> = match name {
-                    Token::W(Word { inner: "" }) => None,
-                    _ => Some(name),
+                let (ty, rest) = match inpt_step::<Token>(rest) {
+                    InptStep { data: Ok(ty), rest } => (ty, rest),
+                    _ => bail!("could not parse section type"),
+                };
+                let name: Option<_> = if rest.is_empty() {
+                    None
+                } else {
+                    match inpt::<Token>(rest) {
+                        Ok(name) => Some(name),
+                        _ => bail!("could not parse section name"),
+                    }
                 };
                 Line::Section { ty, name }
             }
             "option" => {
                 let (option, value): (Token, Token) =
-                    inpt(rest).map_err(|err| eyre!("could not parse option: {err}"))?;
+                    inpt(rest).map_err(|err| error!("could not parse option: {err}"))?;
                 Line::Option { option, value }
             }
             "list" => {
                 let (list, item): (Token, Token) =
-                    inpt(rest).map_err(|err| eyre!("could not parse list: {err}"))?;
+                    inpt(rest).map_err(|err| error!("could not parse list: {err}"))?;
                 Line::List { list, item }
             }
             kw => bail!("unknown UCI keyword {kw:?}"),
@@ -107,20 +145,39 @@ impl<'a> Line<'a> {
     }
 }
 
-pub type Bump = bumpalo::Bump;
 pub type Lines<'a> = Vec<Line<'a>>;
+pub type Arena = typed_arena::Arena<String>;
 
-pub trait Section<'a>: Sized {
-    fn read(lines: &mut Lines<'a>, index: usize) -> Result<Self, Error>;
-    fn write(&self, lines: &mut Lines<'a>, bump: &'a Bump, index: usize) -> Result<(), Error>;
-    fn append(&self, lines: &mut Lines<'a>, bump: &'a Bump) -> Result<(), Error>;
+pub trait UciSection<'a>: Sized {
+    fn read(lines: &Lines<'a>, index: usize) -> Result<Self, Error>;
+    fn write(&self, lines: &mut Lines<'a>, arena: &'a Arena, index: usize) -> Result<(), Error>;
+    fn append(
+        &self,
+        lines: &mut Lines<'a>,
+        arena: &'a Arena,
+        name: Option<&'a str>,
+    ) -> Result<(), Error>;
+}
+
+pub fn parse_config<V>(
+    path: impl AsRef<Path>,
+    with: impl FnOnce(Lines<'_>) -> Result<V, Error>,
+) -> Result<V, Error> {
+    let text = fs::read_to_string(path)?;
+    with(parse_config_string(&text)?)
+}
+
+pub fn parse_config_string(config: &str) -> Result<Lines<'_>, Error> {
+    config.lines().map(Line::parse).collect()
 }
 
 /// TODO: async version?
 pub fn rewrite_config<V>(
     path: impl AsRef<Path>,
-    with: impl for<'a> FnOnce(&mut Lines<'a>, &'a Bump) -> Result<V, Error>,
+    with: impl for<'a> FnOnce(&mut Lines<'a>, &'a Arena) -> Result<V, Error>,
 ) -> Result<V, Error> {
+    use std::io::Write;
+
     use fd_lock_rs::{FdLock, LockType};
     use std::io::BufReader;
     let file = File::options()
@@ -131,16 +188,187 @@ pub fn rewrite_config<V>(
         .open(path)?;
     let mut locked = FdLock::lock(file, LockType::Exclusive, true)?;
     let mut lines = Vec::new();
-    let bump = Bump::new();
+    let arena = Arena::new();
     for line in BufReader::new(&mut *locked).lines() {
-        let line = bump.alloc_str(&line?); // TODO: no extra alloc?
+        let line = arena.alloc(line?);
         lines.push(Line::parse(line)?);
     }
-    let v = with(&mut lines, &bump)?;
+    let v = with(&mut lines, &arena)?;
     locked.set_len(0)?;
     let mut writer = BufWriter::new(&mut *locked);
     for line in lines {
         write!(writer, "{}", line)?;
     }
     Ok(v)
+}
+
+pub fn rewrite_config_string(
+    config: String,
+    with: impl for<'a> FnOnce(&mut Lines<'a>, &'a Arena) -> Result<(), Error>,
+) -> Result<String, Error> {
+    use std::fmt::Write;
+
+    let mut lines = Vec::new();
+    let arena = Arena::new();
+    for line in arena.alloc(config).lines() {
+        lines.push(Line::parse(line)?);
+    }
+    with(&mut lines, &arena)?;
+    let mut writer = String::new();
+    for line in lines {
+        write!(writer, "{}", line)?;
+    }
+    Ok(writer)
+}
+
+#[test]
+fn test_read_section() {
+    let original = r"
+config bar
+    option always 0
+    option yes 1
+    list many 2
+    list many 3
+    list many 4
+";
+
+    let expected = Bar {
+        always: 0,
+        yes: Some(1),
+        no: None,
+        many: vec![2, 3, 4],
+    };
+
+    #[derive(UciSection, PartialEq, Eq, Debug)]
+    struct Bar {
+        always: i32,
+        yes: Option<i32>,
+        no: Option<i32>,
+        many: Vec<i32>,
+    }
+
+    let lines = parse_config_string(original).unwrap();
+    let parsed = Bar::read(&lines, 1).unwrap();
+
+    println!(
+        "===Original==={original}===Parsed===\n{parsed:#?}\n===Expected===\n{expected:#?}\n====="
+    );
+    assert_eq!(parsed, expected);
+}
+
+#[test]
+fn test_append_section() {
+    let original = r"
+config foo
+    option hello world
+    # a comment here
+";
+
+    let expected = r"
+config foo
+    option hello world
+    # a comment here
+
+config bar appended
+    option always 0
+    option yes 1
+    list many 2
+    list many 3
+    list many 4
+";
+
+    #[derive(UciSection)]
+    struct Bar {
+        always: i32,
+        yes: Option<i32>,
+        no: Option<i32>,
+        many: Vec<i32>,
+    }
+
+    let edited = rewrite_config_string(original.to_string(), |lines, arena| {
+        Bar {
+            always: 0,
+            yes: Some(1),
+            no: None,
+            many: vec![2, 3, 4],
+        }
+        .append(lines, arena, Some("appended"))
+    })
+    .unwrap();
+
+    println!("===Original==={original}===Edited==={edited}===Expected==={expected}=====");
+    assert_eq!(edited.replace("\t", "    "), expected);
+}
+
+#[test]
+fn test_edit_section() {
+    let original = r"
+# top comment
+config bar named
+    # always comment
+    option always 0
+
+    # no comment
+    option no 1
+
+    # many comment
+    list many 2
+
+    # few comment
+    list few 3
+    list few 4
+    list few 5
+
+    # ignored comment
+    option ignored 6
+
+# bottom comment
+";
+
+    let expected = r"
+# top comment
+config bar named
+    # always comment
+    option always 0
+
+    # no comment
+
+    # many comment
+    list many 2
+
+    # few comment
+    list few 5
+    option yes 1
+    list many 3
+    list many 4
+
+    # ignored comment
+    option ignored 6
+
+# bottom comment
+";
+
+    #[derive(UciSection)]
+    struct Bar {
+        always: i32,
+        yes: Option<i32>,
+        no: Option<i32>,
+        many: Vec<i32>,
+        few: Vec<i32>,
+    }
+
+    let edited = rewrite_config_string(original.to_string(), |lines, arena| {
+        Bar {
+            always: 0,
+            yes: Some(1),
+            no: None,
+            many: vec![2, 3, 4],
+            few: vec![5],
+        }
+        .write(lines, arena, 2)
+    })
+    .unwrap();
+
+    println!("===Original==={original}===Edited==={edited}===Expected==={expected}=====");
+    assert_eq!(edited.replace("\t", "    "), expected);
 }
--- uciedit_macros/src/lib.rs
+++ uciedit_macros/src/lib.rs
@@ -1,14 +1,321 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use darling::FromDeriveInput;
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote, ToTokens};
+use syn::{parse_macro_input, parse_quote, Data, DeriveInput, GenericArgument, Ident, Path, Type};
+
+#[derive(FromDeriveInput, Default)]
+#[darling(default, attributes(uci))]
+struct UciSectionOpts {
+    ty: Option<String>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct UciField {
+    placehold: Ident,
+    field: Ident,
+    name: String,
+    is_opt: bool,
+    is_vec: bool,
+    crat: Path,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl UciField {
+    fn read_decl(&self) -> TokenStream {
+        let UciField { placehold, .. } = self;
+        if self.is_vec {
+            quote!(let mut #placehold = Vec::new();)
+        } else {
+            quote!(let mut #placehold = None;)
+        }
     }
+
+    fn read_option_arm(&self) -> TokenStream {
+        if self.is_vec {
+            return TokenStream::new();
+        }
+        let UciField {
+            placehold, name, ..
+        } = self;
+        quote! {
+            #name if #placehold.is_none() => #placehold = Some(std::str::FromStr::from_str(&value.as_str())?),
+        }
+    }
+
+    fn read_list_arm(&self) -> TokenStream {
+        if !self.is_vec {
+            return TokenStream::new();
+        }
+        let UciField {
+            placehold, name, ..
+        } = self;
+        quote! {
+            #name => #placehold.push(std::str::FromStr::from_str(&item.as_str())?),
+        }
+    }
+
+    fn read_init(&self) -> TokenStream {
+        let UciField {
+            placehold,
+            field,
+            crat,
+            ..
+        } = self;
+        if self.is_opt || self.is_vec {
+            quote! { #field: #placehold, }
+        } else {
+            let msg = format!("missing field {}", field);
+            quote! { #field: #placehold.ok_or(#crat::error!(#msg))?, }
+        }
+    }
+
+    fn write_decl(&self) -> TokenStream {
+        let UciField {
+            placehold,
+            field,
+            crat,
+            name,
+            ..
+        } = self;
+        match (self.is_opt, self.is_vec) {
+            (false, false) => quote! {
+                let mut #placehold = Some(#crat::Line::Option {
+                    option: #crat::Token::from_str(#name, arena),
+                    value: #crat::Token::from_display(&self.#field, arena),
+                })
+                .into_iter();
+            },
+            (true, false) => quote! {
+                let mut #placehold = self.#field.iter().map(|value| #crat::Line::Option {
+                    option: #crat::Token::from_str(#name, arena),
+                    value: #crat::Token::from_display(value, arena),
+                });
+            },
+            (false, true) => quote! {
+                let mut #placehold = self.#field.iter().map(|item| #crat::Line::List {
+                    list: #crat::Token::from_str(#name, arena),
+                    item: #crat::Token::from_display(item, arena),
+                });
+            },
+            _ => panic!("can not be both Option and Vec"),
+        }
+    }
+
+    fn write_option_arm(&self) -> TokenStream {
+        if self.is_vec {
+            return TokenStream::new();
+        }
+        let UciField {
+            placehold, name, ..
+        } = self;
+        quote! {
+            #name => #placehold.next(),
+        }
+    }
+
+    fn write_list_arm(&self) -> TokenStream {
+        if !self.is_vec {
+            return TokenStream::new();
+        }
+        let UciField {
+            placehold, name, ..
+        } = self;
+        quote! {
+            #name => #placehold.next(),
+        }
+    }
+}
+
+fn chained_write_iters(fields: &[UciField]) -> Option<TokenStream> {
+    let mut chained = None;
+    for UciField { placehold, .. } in fields.iter().rev() {
+        chained = Some(match chained {
+            Some(joined) => quote! { #placehold.chain(#joined) },
+            None => quote! { #placehold },
+        });
+    }
+    chained
+}
+
+fn read_body(fields: &[UciField], struc: Ident, _ty: String, crat: Path) -> TokenStream {
+    let decl = fields.iter().map(UciField::read_decl);
+    let option_arm = fields.iter().map(UciField::read_option_arm);
+    let list_arm = fields.iter().map(UciField::read_list_arm);
+    let init = fields.iter().map(UciField::read_init);
+    quote! {
+        let Some(#crat::Line::Section { .. }) = lines.get(index) else {
+            bail!("line {index} does not start a section")
+        };
+        #(#decl)*
+
+        loop {
+            index += 1;
+            match lines.get(index) {
+                Some(#crat::Line::Option { option, value }) => match &*option.as_str() {
+                    #(#option_arm)*
+                    _ => continue,
+                },
+                Some(#crat::Line::List { list, item }) => match &*list.as_str() {
+                    #(#list_arm)*
+                    _ => continue,
+                },
+                None | Some(#crat::Line::Section { .. }) => break,
+                _ => continue,
+            }
+        }
+
+        Ok(#struc {
+            #(#init)*
+        })
+    }
+}
+
+fn write_body(fields: &[UciField], _struc: Ident, ty: String, crat: Path) -> TokenStream {
+    let decl = fields.iter().map(UciField::write_decl);
+    let option_arm = fields.iter().map(UciField::write_option_arm);
+    let list_arm = fields.iter().map(UciField::write_list_arm);
+    let chain = chained_write_iters(fields);
+    let not_section_err = format!("line {{index}} is not a {ty} section");
+    quote! {
+        let Some(#crat::Line::Section { ty, .. }) = lines.get(index) else {
+            bail!("line {index} does not start a section")
+        };
+        if ty.as_str() != #ty {
+            bail!(#not_section_err)
+        }
+
+        #(#decl)*
+
+        let mut insert_at = index + 1;
+        loop {
+            index += 1;
+            let Some(line) = lines.get_mut(index) else {
+                break;
+            };
+            *line = match line {
+                #crat::Line::Option { option, .. } => match &*option.as_str() {
+                    #(#option_arm)*
+                    _ => continue,
+                },
+                #crat::Line::List { list, .. } => match &*list.as_str() {
+                    #(#list_arm)*
+                    _ => continue,
+                },
+                #crat::Line::Section { .. } => break,
+                _ => continue,
+            }
+            .unwrap_or(#crat::Line::Skip);
+            insert_at = index + 1;
+        }
+
+        lines.splice(
+            insert_at..insert_at,
+            #chain,
+        );
+
+        Ok(())
+    }
+}
+
+fn append_body(fields: &[UciField], _struc: Ident, ty: String, crat: Path) -> TokenStream {
+    let decl = fields.iter().map(UciField::write_decl);
+    let chain = chained_write_iters(fields);
+    quote! {
+        #(#decl)*
+
+        if !lines.is_empty() {
+            lines.push(#crat::Line::Empty);
+        }
+
+        lines.push(#crat::Line::Section {
+            ty: #crat::Token::from_str(#ty, arena),
+            name: name.map(|n| #crat::Token::from_str(n, arena)),
+        });
+        lines.extend(#chain);
+
+        Ok(())
+    }
+}
+
+fn is_collection_with_generic(ty: &Type, collection: &str) -> bool {
+    if let Type::Path(path) = ty {
+        if let Some(segment) = path.path.segments.first() {
+            if segment.ident == collection {
+                if let syn::PathArguments::AngleBracketed(args) = &segment.arguments {
+                    if args.args.len() == 1 {
+                        if let GenericArgument::Type(_) = args.args[0] {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    false
+}
+
+#[proc_macro_derive(UciSection, attributes(uci))]
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let opts = UciSectionOpts::from_derive_input(&input).expect("Wrong options");
+
+    let crat: Path = parse_quote! { ::uciedit };
+    let struc = input.ident;
+    let ty = opts.ty.unwrap_or(struc.to_string().to_lowercase());
+    let Data::Struct(struct_data) = input.data else {
+        panic!("only structs are supported")
+    };
+    let fields: Vec<_> = struct_data
+        .fields
+        .into_iter()
+        .map(|f| {
+            let i = f.ident.unwrap();
+            UciField {
+                placehold: format_ident!("field_{}", i),
+                field: i.clone(),
+                name: i.to_string(),
+                is_opt: is_collection_with_generic(&f.ty, "Option"),
+                is_vec: is_collection_with_generic(&f.ty, "Vec"),
+                crat: crat.clone(),
+            }
+        })
+        .collect();
+
+    let read_body = read_body(&fields, struc.clone(), ty.clone(), crat.clone());
+    let write_body = write_body(&fields, struc.clone(), ty.clone(), crat.clone());
+    let append_body = append_body(&fields, struc.clone(), ty.clone(), crat.clone());
+
+    let (_, type_generics, where_clause) = input.generics.split_for_impl();
+    let mut lt_generics = input.generics.clone();
+    lt_generics.params.push(parse_quote! { 'a });
+    let (impl_generics, _, _) = lt_generics.split_for_impl();
+
+    quote! {
+        impl #impl_generics #crat::UciSection<'a> for #struc #type_generics #where_clause {
+            fn read(lines: &#crat::Lines<'a>, mut index: usize) -> Result<Self, #crat::Error> {
+                #read_body
+            }
+
+            #[allow(unused_mut)]
+            fn write(
+                &self,
+                lines: &mut #crat::Lines<'a>,
+                arena: &'a #crat::Arena,
+                mut index: usize,
+            ) -> Result<(), #crat::Error> {
+                #write_body
+            }
+
+            #[allow(unused_mut)]
+            fn append(
+                &self,
+                lines: &mut #crat::Lines<'a>,
+                arena: &'a #crat::Arena,
+                name: Option<&'a str>,
+            ) -> Result<(), #crat::Error> {
+                #append_body
+            }
+        }
+
+    }
+    .into_token_stream()
+    .into()
 }
